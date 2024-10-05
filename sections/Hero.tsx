af35ee69679a26b4b660ddf054947c5875d4874a
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-10-05 18:12:29 -0400

    refactor: hero

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -6,6 +6,11 @@ interface Props {
   content?: string;
 }
 
-export default function Hero({ content = "<h1>Title</h1>" }: Props) {
-  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+export default function Hero({ content = "<h1>SandBox</h1>" }: Props) {
+  return (
+    <div
+      class="w-full flex justify-center bg-black h-screen text-white text-5xl items-center"
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  );
 }
